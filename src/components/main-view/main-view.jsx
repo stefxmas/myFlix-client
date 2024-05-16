d29d6c9ca1668e072aae466ddf231a6d6dfbd718
stefxmas <143958588+stefxmas@users.noreply.github.com>
2024-05-16 14:27:25 -0400

    changed json.
    json, movie card and main view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,45 +3,10 @@ import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-  const [movies, setmovies] = useState([
-    {
-      id: 1,
-      title: "Silence of the Lambs",
-      image:
-      "https://www.indiewire.com/wp-content/uploads/2017/02/silence-of-the-lambs-anthony-hopkins-01.jpg?w=1330&h=748&crop=1",
-      author: "Jonathan Demme"
-    },
-    {
-      id: 2,
-      title: "Avengers Infinity War",
-      image:
-      "https://m.media-amazon.com/images/I/81V1KTnYKwL._SY500_.jpg",
-      author: "Joe Russo"
-    },
-    {
-      id: 3,
-      title: "John Wick",
-      image:
-      "https://hips.hearstapps.com/hmg-prod/images/keanu-reeves-john-wick-4-1677167115.jpg?resize=980:*",
-      author: "Chad Stahelski"
-    },
-    {
-      id: 4,
-      title: "The Shining",
-      image:
-      "https://m.media-amazon.com/images/I/91yYss6rrXL._SL1500_.jpg",
-      author: "Stanley Kubrick"
-    },
-    {
-      id: 5,
-      title: "Harold & Kumar Go to White Castle",
-      image:
-      "https://akns-images.eonline.com/eol_images/Entire_Site/2022224/rs_1024x759-220324102354-1024-John-Cho-Kel-Penn-LT-32422-shutterstock_editorial_5882973u.jpg?fit=around%7C1024:759&output-quality=90&crop=1024:759;center,top",
-      author: "Danny Leiner"
-    }
-  ]);
+  const [movies, setmovies] = useState([])
+
   useEffect(() => {
-    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjQzODc2MDVmNjY0MjI2NGUzZjYxOWUiLCJVc2VybmFtZSI6ImpvaG5kb2UxMjExMiIsIlBhc3N3b3JkIjoiJDJiJDEwJEdEWDJkVXgubnM0a2RwN3UyLlZoZy4xb2p3OGRsUkduUDlyN3ZFbXNoOHN4elJYT1JEdUYuIiwiRW1haWwiOiJqb2huZG9lMTIxMkBlbWFpbC5jb20iLCJCaXJ0aGRheSI6IjE5NzAtMTEtMTZUMDA6MDA6MDAuMDAwWiIsIkZhdm9yaXRlTW92aWVzIjpbXSwiX192IjowLCJpYXQiOjE3MTU3MDE2MDYsImV4cCI6MTcxNjMwNjQwNiwic3ViIjoiam9obmRvZTEyMTEyIn0.1_RkR5q2j4Xbfeh5ts3tojuPQK4Nns9uJISusNiUIZU'
+    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjQzODc2MDVmNjY0MjI2NGUzZjYxOWUiLCJVc2VybmFtZSI6ImpvaG5kb2UxMjExMiIsIlBhc3N3b3JkIjoiJDJiJDEwJEdEWDJkVXgubnM0a2RwN3UyLlZoZy4xb2p3OGRsUkduUDlyN3ZFbXNoOHN4elJYT1JEdUYuIiwiRW1haWwiOiJqb2huZG9lMTIxMkBlbWFpbC5jb20iLCJCaXJ0aGRheSI6IjE5NzAtMTEtMTZUMDA6MDA6MDAuMDAwWiIsIkZhdm9yaXRlTW92aWVzIjpbXSwiX192IjowLCJpYXQiOjE3MTU4ODM5MTUsImV4cCI6MTcxNjQ4ODcxNSwic3ViIjoiam9obmRvZTEyMTEyIn0.kNVWr3lwvgqwN7T7IiIpuMvAaJFZ79LEkzu3Dx7_Sb8'
     fetch("https://ajs-movie-api-598adfef849b.herokuapp.com/movies", {
       headers: { Authorization: `Bearer ${token}` }
     })
