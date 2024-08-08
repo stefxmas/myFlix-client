53190e4bfb01870be210aa8f26b7dff3ddf2bcfb
stefxmas <143958588+stefxmas@users.noreply.github.com>
2024-08-08 11:40:16 -0400

    added scss instead of css

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { MainView } from './components/main-view/main-view';
 
 // Import statement to indicate that you need to bundle `./index.scss`
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.scss";
 import Container from 'react-bootstrap/Container';
 
@@ -17,7 +17,7 @@ import Container from 'react-bootstrap/Container';
 // };
 const App = () => {
   return (
-    <Container style={{border: "1px solid red"}}>
+    <Container>
     <MainView />
   </Container>
   );
